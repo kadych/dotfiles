b90c02a9d9ec3092b5ad74a711cd58e16fbd957b
sbt-dunaev-ka <KADunaev.SBT@sberbank.ru>
2015-11-10 16:41:32 +0300

    added hidden attr for created directories

--- install.bat
+++ install.bat
@@ -3,11 +3,14 @@ set VUNDLE_HOME=%USERPROFILE%\.vundle
 set VUNDLE_REPO=https://github.com/VundleVim/Vundle.vim.git
 if not exist "%USERPROFILE%\.vim" (
     mklink /j "%USERPROFILE%\.vim" .vim
+    attrib +h /l "%USERPROFILE%\.vim"
 )
 if not exist "%USERPROFILE%\vimfiles" (
     mklink /j "%USERPROFILE%\vimfiles" .vim
+    attrib +h /l "%USERPROFILE%\vimfiles"
 )
 if not exist "%VUNDLE_HOME%" (
     "%GIT_CMD%" clone %VUNDLE_REPO% "%VUNDLE_HOME%/Vundle.vim"
+    attrib +h "%VUNDLE_HOME%"
 )
 vim +PluginInstall +qall
