09176b0fbf61a559b7ceddd407f804173a4f615a
Kirill Dunaev <kirill.dunaev@gmail.com>
2015-12-17 13:09:43 +0300

    new pudb config

---
+++ pudb/xoria256.py
@@ -0,0 +1,125 @@
+palette.update({
+    "header": ("h252", "h233", "standout"),
+
+    # {{{ variables view
+    "variables": ("h234", "h234"),
+    "variable separator": ("h23", "h252"),
+
+    "var label": ("h110", "h234"),
+    "var value": ("h252", "h234"),
+    "focused var label": ("h234,bold", "h110"),
+    "focused var value": ("h234", "h110"),
+
+    "highlighted var label": ("h252", "h22"),
+    "highlighted var value": ("h252", "h22"),
+    "focused highlighted var label": ("h252", "h110"),
+    "focused highlighted var value": ("h234", "h110"),
+
+    "return label": ("h110", "h234"),
+    "return value": ("h110", "h234"),
+    "focused return label": ("h234,bold", "h110"),
+    "focused return value": ("h234", "h110"),
+    # }}}
+
+    # {{{ stack view
+    "stack": ("h234", "h234"),
+
+    "frame name": ("h252,bold", "h234"),
+    "frame class": ("h252", "h234"),
+    "frame location": ("h252", "h234"),
+
+    "focused frame name": ("h234,bold", "h110"),
+    "focused frame class": ("h234", "h110"),
+    "focused frame location": ("h234", "h110"),
+
+    "current frame name": ("h110,bold", "h234"),
+    "current frame class": ("h110", "h234"),
+    "current frame location": ("h110", "h234"),
+
+    "focused current frame name": ("h234,bold", "h110"),
+    "focused current frame class": ("h234", "h110"),
+    "focused current frame location": ("h234", "h110"),
+    # }}}
+
+    # {{{ breakpoint view
+    "breakpoint": ("h174", "h234"),
+    "focused breakpoint": ("h234", "h110"),
+
+    "current breakpoint": ("h174,bold", "h234"),
+    "focused current breakpoint": ("h234,bold", "h110"),
+    # }}}
+
+    # {{{ ui widgets
+
+    "selectable": ("h252", "h234"),
+    "focused selectable": ("h252,bold", "h234"),
+
+    "button": ("h248", "h237"),
+    "focused button": ("h234,bold", "h110"),
+
+    "background": ("h248", "h235"),
+    "hotkey": ("h252", "h234", "standout"),
+    "focused sidebar": ("h248", "h237"),
+
+    "warning": ("h252", "h124", "standout"),
+
+    "label": ("h248", "h235"),
+    "value": ("h252", "h234"),
+    "fixed value": ("h252", "h235"),
+    "group head": ("h25", "h252"),
+
+    "search box": ("h252", "h234"),
+    "search not found": ("h252", "h124"),
+
+    "dialog title": ("h252,bold", "h235"),
+
+    # }}}
+
+    # {{{ source view
+    "breakpoint marker": ("h174", "h233"),
+
+    "breakpoint source": ("h234", "h174"),
+    "breakpoint focused source": ("h234,bold", "h174"),
+    "current breakpoint source": ("h234", "h174"),
+    "current breakpoint focused source": ("h234,bold", "h174"),
+    # }}}
+
+    # {{{ highlighting
+    "source": ("h252", "h234"),
+    "focused source": ("h252", "h235"), # changed
+    "highlighted source": ("h252", "h22"),
+    "current source": ("h234", "h110"), # dark cyan background
+    "current focused source": ("h234,bold", "h110"),
+    "current highlighted source": ("h234,bold", "h110"),
+
+    "line number": ("h241", "h233"),
+    "keyword": ("h110", "h234"),
+
+    "literal": ("h180", "h234"),
+    "string": ("h229", "h234"),
+    "doublestring": ("h229", "h234"),
+    "singlestring": ("h229", "h234"),
+    "docstring": ("h229", "h234"),
+
+    "name": ("h182", "h234"),
+    "punctuation": ("h252", "h234"),
+    "comment": ("h241", "h234"),
+
+    # }}}
+
+    # {{{ shell
+    "command line edit": ("h252", "h234"),
+    "command line prompt": ("h180", "h234"),
+
+    "command line output": ("h110", "h234"),
+    "command line input": ("h252", "h234"),
+    "command line error": ("h174", "h234"),
+
+    "focused command line output": ("h192", "h24"),
+    "focused command line input": ("h252", "h24"),
+    "focused command line error": ("h234", "h24"),
+
+    "command line clear button": ("h252", "h234"),
+    "command line focused button": ("h252", "h24"),
+    # }}}
+})
