7d07738e6aebad5ca01d05cb1efc234696803d7a
Kirill Dunaev <kirill.dunaev@gmail.com>
2016-03-01 15:02:16 +0300

    fixed: link to a number of files/directories

---
+++ system/fzf.bash
@@ -0,0 +1,20 @@
+# Setup fzf
+# ---------
+if [[ ! "$PATH" == */home/kirill/.fzf/bin* ]]; then
+  export PATH="$PATH:/home/kirill/.fzf/bin"
+fi
+
+# Man path
+# --------
+if [[ ! "$MANPATH" == */home/kirill/.fzf/man* && -d "/home/kirill/.fzf/man" ]]; then
+  export MANPATH="$MANPATH:/home/kirill/.fzf/man"
+fi
+
+# Auto-completion
+# ---------------
+[[ $- == *i* ]] && source "/home/kirill/.fzf/shell/completion.bash" 2> /dev/null
+
+# Key bindings
+# ------------
+source "/home/kirill/.fzf/shell/key-bindings.bash"
+
