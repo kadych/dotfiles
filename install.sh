4c85d7378852e5ed8bd828585d99761540b7dc97
Kirill Dunaev <kirill.dunaev@gmail.com>
2016-03-14 15:09:34 +0300

    always yes on dotty.py questions during install

--- install.sh
+++ install.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 git submodule init
 git submodule update
-python3 dotty/dotty.py dotfiles.yaml
+yes | python3 dotty/dotty.py dotfiles.yaml
 pip3 install --user neovim
 nvim -c PlugInstall -c qa
