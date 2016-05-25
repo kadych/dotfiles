dc9b2193f7ee0b5b55cca291b91fc3ed4b72fd18
Kirill Dunaev <kirill.dunaev@gmail.com>
2016-05-25 23:52:08 +0300

    Разные доработки в части tmux+nvim

---
+++ dotfiles.js
@@ -0,0 +1,50 @@
+{
+    "directories": [
+        "~/.config",
+        "~/.config/xfce4",
+        "~/.config/xfce4/terminal",
+        "~/.bundle"
+    ],
+
+    "commands": [
+    ],
+
+    "link": {
+        "system/profile": "~/.profile",
+        "system/bashrc": "~/.bashrc",
+        "system/bash_logout": "~/.bash_logout",
+        "system/functionrc": "~/.functionrc",
+        "system/aliasrc": "~/.aliasrc",
+        "system/promptrc": "~/.promptrc",
+        "system/envrc": "~/.envrc",
+        "system/backgroundrc": "~/.backgroundrc",
+        "system/inputrc": "~/.inputrc",
+        "system/tmux.conf": "~/.tmux.conf",
+        "system/tmux-light.conf": "~/.tmux-light.conf",
+        "system/tmux-dark.conf": "~/.tmux-dark.conf",
+        "system/fzfrc": "~/.fzfrc",
+        "system/yank.sh": "~/.yank.sh",
+
+        "vim": [
+            "~/.config/nvim",
+            "~/.vim"
+        ],
+        "vim/vimrc": "vim/init.vim",
+
+        "pudb": "~/.config/pudb",
+
+        "ipython": "~/.ipython",
+
+        "desktop/terminalrc": "~/.config/xfce4/terminal/terminalrc",
+        "desktop/accels.scm": "~/.config/xfce4/terminal/accels.scm",
+        "desktop/Xdefaults": "~/.Xdefaults",
+        "desktop/xinputrc": "~/.xinputrc",
+        "desktop/Xmodmap": "~/.Xmodmap",
+
+        "misc/ackrc": "~/.ackrc",
+        "misc/ctags.cnf": "~/.ctags",
+        "misc/pylintrc": "~/.pylintrc",
+        "misc/gitconfig": "~/.gitconfig",
+        "misc/kdiff3rc": "~/.kdiff3rc"
+    }
+}
--- install.sh
+++ install.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 git submodule init
 git submodule update
-yes | python3 dotty/dotty.py dotfiles.yaml
-pip3 install --user neovim
+yes | dotty/dotty.py dotfiles.js
+pip install --user neovim
 nvim -c PlugInstall -c qa
 source ~/.bashrc
