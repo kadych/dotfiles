6a893db0875e96d49471a5c8bae665b1039ca64c
Kirill Dunaev <kdunaev@yandex-team.ru>
2018-03-20 15:04:44 +0300

    commit

--- ipython/profile_default/ipython_config.py
+++ ipython/profile_default/ipython_config.py
@@ -1,4 +1,5 @@
 c = get_config()
 c.TerminalIPythonApp.display_banner = False
 c.InteractiveShell.confirm_exit = False
+c.InteractiveShell.colors = 'linux'
 c.TerminalInteractiveShell.editor = 'nvim'
