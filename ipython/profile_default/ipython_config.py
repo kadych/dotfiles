0278ec5bca7f454dcbd4d6b20736bb86b4659b02
Kirill Dunaev <kdunaev@yandex-team.ru>
2019-06-18 11:03:05 +0300

    some changes

--- ipython/profile_default/ipython_config.py
+++ ipython/profile_default/ipython_config.py
@@ -1,5 +1,4 @@
 c = get_config()
 c.TerminalIPythonApp.display_banner = False
 c.InteractiveShell.confirm_exit = False
-c.InteractiveShell.colors = 'linux'
 c.TerminalInteractiveShell.editor = 'nvim'
